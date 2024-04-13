d68e18abded3c2e6b06c529aadc199db93040c2f
Pavel <rezusx@mail.ru>
2024-04-13 20:08:33 +0300

    fix

---
+++ ToDoList/CategoryViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  CategoryViewController2.swift
+//  ToDoList
+//
+//  Created by Pavel Dolgopolov on 13.04.2024.
+//
+
+import UIKit
+
+class CategoryViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
